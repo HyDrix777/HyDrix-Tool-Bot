59bdf71da9978d770aec3af6108b8b4c51474c62
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-29 20:55:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 from pyrogram import Client
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "6"))
-API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5172677053:AAE0dIBBQKCPc5FJeGvAYcgPJLh1_cud4bY")
+API_ID = int(os.environ.get("API_ID", "18891187"))
+API_HASH = os.environ.get("API_HASH", "7d120384f48b2a86fa2b9e9772a28af6")
 
 if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
     Hydrix=Client(
-        "Pyrogram Bot",
+       "Pyrogram Bot",
         bot_token=BOT_TOKEN,
         api_hash=API_HASH,
         api_id=API_ID,
         plugins=plugins
-    )
-    Hydrix.run()
+)
+
+Hydrix.run()
