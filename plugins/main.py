be76802ad28988e187a33d159359153f91946c09
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-30 14:23:04 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -59,7 +59,7 @@ async def stickers(_, message):
 
 # Service clear
 
-@Client.on_message(filters.regex("http") | filters.regex("t.me") | filters.regex("youtu.be") | filters.regex("com") | filters.regex("https") | filters.regex("!" ) | filters.service)
+@Client.on_message(filters.command("http") | filters.regex("t.me") | filters.regex("youtu.be") | filters.regex("com") | filters.regex("https") | filters.regex("!" ) | filters.service)
 async def delete(bot,message):
  await message.delete()
 
