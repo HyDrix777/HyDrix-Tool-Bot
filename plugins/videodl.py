3d03505100f604012e793f084ea5be06184e55b7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-30 20:43:16 +0530

    Update videodl.py

--- plugins/videodl.py
+++ plugins/videodl.py
@@ -9,7 +9,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
-from driver.decorators import humanbytes
 from driver.filters import command, other_filters
 
 
