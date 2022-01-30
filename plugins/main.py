ad09a564afa169261deb5698bf410c96bc469476
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 00:14:11 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -152,3 +152,14 @@ async def forwarded(_, msg):
             else:
                 text += f'ID : `{msg.forward_from_chat.id}`'
             await msg.reply(text, quote=True)
+
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot, msg):
+    bot_id = (await bot.get_me())["id"]
+    members = msg.new_chat_members
+    for member in members:
+        if member.id == bot_id:
+            await msg.reply(
+                f"Thanks for adding me here! \n\nThis group's ID is `{msg.chat.id}`"
+            )
