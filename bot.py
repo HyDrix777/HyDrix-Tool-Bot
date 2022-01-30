578a967f4e72b3fce59800cbc9fcb34022817a78
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 00:48:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5139527038:AAF_vx72GV0xK9GvOg3ut3vIF-J6zxlmGGE")
 API_ID = int(os.environ.get("API_ID", "18891187"))
 API_HASH = os.environ.get("API_HASH", "7d120384f48b2a86fa2b9e9772a28af6")
+MUST_JOIN = os.environ.get("MUST_JOIN", "Music_Galaxy_Dl")
 
 if __name__ == "__main__" :
     plugins = dict(
@@ -14,6 +15,7 @@ if __name__ == "__main__" :
         bot_token=BOT_TOKEN,
         api_hash=API_HASH,
         api_id=API_ID,
+        must_join=MUST_JOIN,
         plugins=plugins
    )
 
