4a148c5d5813b311a144ddf33ba4b380c20284b6
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-30 12:31:14 +0530

    Update gtranslator.py

--- plugins/gtranslator.py
+++ plugins/gtranslator.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputTextMessageContent, InlineQueryResultArticle
 from gpytranslate import Translator
 import sqlite3, string
 
