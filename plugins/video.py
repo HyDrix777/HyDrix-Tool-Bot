5cbe3de4c1625b3e36d2e4bf82e01a56fb626444
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 13:47:40 +0530

    Update video.py

--- plugins/video.py
+++ plugins/video.py
@@ -21,7 +21,7 @@ def a(client, message):
         query += ' ' + str(i)
     print(query)
     m = message.reply('🔎Searching..your request...')
-    ydl_opts = {"format": "bestvideo[ext=3gp]"}
+    ydl_opts = {"format": "bestvideo[ext=mp4]"}
     try:
         results = []
         count = 0
