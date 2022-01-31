d27c6a3268bddcb75ffd922393e0b750cd9c9d3d
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 17:11:44 +0530

    Update gtransulator.py

--- plugins/gtransulator.py
+++ plugins/gtransulator.py
@@ -1,21 +1,8 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from googletrans import Translator
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from .list import list
-from database.gtrans_mdb import find_one
+
 
 @Client.on_message(filters.command(["tr"]))
 async def left(client,message):
