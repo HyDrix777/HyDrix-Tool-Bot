aef7b3a688ea0f0572aad6d0fe87b1fc21a11738
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 16:52:36 +0530

    Update memes.py

--- plugins/memes.py
+++ plugins/memes.py
@@ -42,26 +42,6 @@ async def throw_dart(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
-# DICE------------
-
-# EMOJI CONSTANTS
-TRY_YOUR_LUCK = "🎳"
-# EMOJI CONSTANTS
-
-@Client.on_message(
-    filters.command(["dice", "cownd"])
-)
-async def luck_cownd(client, message):
-    """ /luck an @animatedluck """
-    rep_mesg_id = message.message_id
-    if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.message_id
-    await client.send_dice(
-        chat_id=message.chat.id,
-        emoji=TRY_YOUR_LUCK,
-        disable_notification=True,
-        reply_to_message_id=rep_mesg_id
-    )
 
 # LUCK------------
 
