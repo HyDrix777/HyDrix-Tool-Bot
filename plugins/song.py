5d60e23c9fedbeab862f2c21fa59d051640ba6d8
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 13:22:16 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -80,3 +80,72 @@ def a(client, message):
         os.remove(thumb_name)
     except Exception as e:
         print(e)
+
+# video download
+
+@Client.on_message(filters.private & filters.command(['v']))
+def a(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎Searching..your request...')
+    ydl_opts = {"format": "bestvideo[ext=mp4]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            channel = results[0]["channel"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+           
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('Found nothing..Try again later...🙁')
+            return
+    except Exception as e:
+        m.edit(
+            "❎ Found nothing.\n\n𝖯𝗅𝖾𝖺𝗌𝖾 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 𝖮𝗋 𝖲𝖾𝖺𝗋𝖼𝗁 𝖺𝗍 Google.com 𝖥𝗈𝗋 𝖢𝗈𝗋𝗋𝖾𝖼𝗍 𝖲𝗉𝖾𝗅𝗅𝗂𝗇𝗀 𝗈𝖿 𝗍𝗁𝖾 song.\n\nEg.`/s Believer`"
+        )
+        print(str(e))
+        return
+    m.edit("📤Uploading To Telegram")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep =  f'🔍<b>Track Downloaded</b>\n\n🎙️ <b>Title:</b> [{title[:35]}]\n\n🔗 <b>Link:</b> ({link})\n⌚ <b>Duration:</b> `{duration}`\n👁️‍🗨️ <b>Views:</b> `{views}`\n🎥 <b>Cʜᴀɴɴᴇʟ:</b> {channel}\n\n⚡<i>Youtube Inline Download Powered By Tg Tool Bot</i>'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=str(info_dict["uploader"]), thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('`Faild Try Again Later`')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
