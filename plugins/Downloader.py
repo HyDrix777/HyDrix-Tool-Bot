a74d76e817fc95b5b0589ede3c73aba56bb5175e
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-01 03:59:09 +0530

    Update Downloader.py

--- plugins/Downloader.py
+++ plugins/Downloader.py
@@ -17,67 +17,6 @@ from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
 
-ydl_opts = {
-    'format': 'best',
-    'keepvideo': True,
-    'prefer_ffmpeg': False,
-    'geo_bypass': True,
-    'outtmpl': '%(title)s.%(ext)s',
-    'quite': True
-}
-
-
-@Client.on_message(filters.command(['song']))
-def song(_, message):
-    query = " ".join(message.command[1:])
-    m = message.reply("🔎 finding song...")
-    ydl_ops = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        duration = results[0]["duration"]
-
-    except Exception as e:
-        m.edit("❌ song not found.\n\nplease give a valid song name.")
-        print(str(e))
-        return
-    m.edit("📥 downloading file...")
-    try:
-        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f"**🎧 Uploader @{bn}**"
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(float(dur_arr[i])) * secmul
-            secmul *= 60
-        m.edit("📤 uploading file...")
-        message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="md",
-            title=title,
-            duration=dur,
-        )
-        m.delete()
-    except Exception as e:
-        m.edit("❌ error, wait for bot owner to fix")
-        print(e)
-
-    try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
-    except Exception as e:
-        print(e)
-
-
 @Client.on_message(filters.command(['vsong']))
 async def vsong(client, message):
     ydl_opts = {
@@ -96,7 +35,7 @@ async def vsong(client, message):
         thumbnail = results[0]["thumbnails"][0]
         thumb_name = f"{title}.jpg"
         thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
+        open(thumb_name).write(thumb.content)
         results[0]["duration"]
         results[0]["url_suffix"]
         results[0]["views"]
