f352470d27f5ae51555e92ad167792bcd6f1a434
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-01-31 17:04:39 +0530

    Update gtransulator.py

--- plugins/gtransulator.py
+++ plugins/gtransulator.py
@@ -1,8 +1,10 @@
-from pyrogram import Translator
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from .list import list
 from database.gtrans_mdb import find_one
+
+
+
 
 @Client.on_message(filters.command(["tr"]))
 async def left(client,message):
