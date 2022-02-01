6b64a9a909536c5694658c5a653bc0bb682ca816
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-01 13:34:35 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -33,9 +33,9 @@ async def start_message(bot, message):
 @Client.on_message(filters.command("help"))
 async def help(bot: Client, message: Message):
     await message.reply_text(
-        text="⍟─────[COM]─────⍟\n🔻My Futures🔻\n\n🖼️➥ **Sticker ID**❓- Just send me the Sticker I would reply with it's Id.\n🗑➥ **Clean Events** - I Can Delete A **Service message** like join left and more,add me your group and promote it.\n📜➥ **Telegraph Uploader** - Send me any **Photo** I'll Upload it into Telegra.ph\n🔎➥ You Can search **YouTube** videos in **nline Mode** | copy this and paste it `@HTGToolBot`\n📹➥🎵⍟ **Mp4** to **Mp3Conveter** - Send a **Video** for converting to **Audio**.\n\n🎼➥ /lyric - Send me a **Song name** I give you a Lyrics.\nEg: `/lyric beggin`\n🎵➥ /s - To **download** audio songs from **YouTube**,You can use this in group.\nEg : `/s Believer`\n📹➥ /v - To **download Video** from **YouTube**, video downloading is very slowly pls wait it.\n⚫➥ **TikTok Download** - Send me **Tiktok** Video **Url** here\n💱➥ **G translator** - You can use me in group's in this command👉🏻 /tr first add me in group.\nClick /list to find your language.\nEg: reply to /tr en 👈🏼\n🕹️➥ **Fun** - Click /fun to get fun commands\n\n🆔➥ **user, group's, bot, channel's Id Finder**\n1. Send any message to get **your ID**.\n2. Forward any message from any user/bot/channel or anonymous admins to get ID.\n3. Add in group / channel to get ID.\n4. **Use /id command:**\n- in private: To get ID through username\n- in group/channel: To get ID of that chat.\n5. **Your DC**❓ - Click /dc to get your DC.\n\n🆎⍟ /about - Know me 🙋\n⍟─────────────⍟",
+        text="⍟─────[ᴄᴏᴍᴍ]─────⍟\n🔻ᴍʏ ғᴜᴛᴜʀᴇ's🔻\n\n🖼️➥ **sᴛɪᴄᴋᴇʀ ɪᴅ**❓- Just send me the Sticker I would reply with it's Id.\n🗑➥ **Clean Events** - I Can Delete A **Service message** like join left and more,add me your group and promote it.\n📜➥ **Telegraph Uploader** - Send me any **Photo** I'll Upload it into Telegra.ph\n🔎➥ You Can search **YouTube** videos in **nline Mode** | copy this and paste it `@HTGToolBot`\n📹➥🎵⍟ **Mp4** to **Mp3Conveter** - Send a **Video** for converting to **Audio**.\n\n🎼➥ /lyric - Send me a **Song name** I give you a Lyrics.\nEg: `/lyric beggin`\n🎵➥ /s - To **download** audio songs from **YouTube**,You can use this in group.\nEg : `/s Believer`\n📹➥ /v - To **download Video** from **YouTube**, video downloading is very slowly pls wait it.\n⚫➥ **TikTok Download** - Send me **Tiktok** Video **Url** here\n💱➥ **G translator** - You can use me in group's in this command👉🏻 /tr first add me in group.\nClick /list to find your language.\nEg: reply to /tr en 👈🏼\n🕹️➥ **Fun** - Click /fun to get fun commands\n\n🆔➥ **user, group's, bot, channel's Id Finder**\n1. Send any message to get **your ID**.\n2. Forward any message from any user/bot/channel or anonymous admins to get ID.\n3. Add in group / channel to get ID.\n4. **Use /id command:**\n- in private: To get ID through username\n- in group/channel: To get ID of that chat.\n5. **Your DC**❓ - Click /dc to get your DC.\n\n🆎⍟ /about - Know me 🙋\n⍟─────────────⍟",
         reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton("👥 Gʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
+           InlineKeyboardButton("👥 ɢʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
            ]]
            )
        )
@@ -45,9 +45,9 @@ async def help(bot: Client, message: Message):
 @Client.on_message(filters.command("list"))
 async def list(bot: Client, message: Message):
     await message.reply_text(
-        text="⍟────List────⍟\n`List is in the form`\n**Language code** => **Language**\n\n`af` ➥ **Afrikaans**\n`sq` -> **Albanian**\n`am` -> **Amharic**\n`ar` -> **Arabic**\n`hy` -> **Armenian**\n`az` -> **Azerbaijani**\n`eu` -> **Basque**\n`be` -> **Belarusian**\n`bn` -> **Bengali**\n`bs` -> **Bosnian**\n`bg` -> **Bulgarian**\n`ca` -> **Catalan**\n`ceb` -> **Cebuano**\n`ny` -> **Chichewa**\n`zh`-`cn` -> **Chinese**\n`co` -> **Corsican**\n`hr` -> **Croatian**\n`cs` -> **Czech**\n`da` -> **Danish**\n`nl` -> **Dutch**\n`en` -> **English**\n`eo` -> **Esperanto**\n`et` -> **Estonian**\n`tl` -> **Filipino**\n`fi` -> **Finnish**\n`fr` -> **French**\n`fy` -> **Frisian**\n`gl` -> **Galician**\n`ka` -> **Georgian**\n`de` -> **German**\n`el` -> **Greek**\n`gu` -> **Gujarati**\n`ht` -> **Haitian creole**\n`ha` -> **Hausa**\n`haw` -> **Hawaiian**\n`iw` -> **Hebrew**\n`hi` -> **Hindi**\n`hmn` -> **Hmong**\n`hu` -> **Hungarian**\n`is` -> **Icelandic**\n`is` -> **Icelandic**\n`ig` -> **Igbo**\n`id` -> **Indonesian**\n`ga` -> **Irish**\n`it` -> **Italian**\n`ja` -> **Japanese*\n`jw` -> **Javanese**\n`kn` -> **Kannada**\n`kk` -> **Kazakh**\n`km` -> **Khmer**\n`rw` -> **Kinyarwanda**\n`ko` -> **Korean**\n`ku` -> **Kurdish (kurmanji)**\n`ky` -> **Kyrgyz**\n`lo` -> **Lao**\n`la` -> **Latin**\n`lv` -> **Latvian**\n`lt` -> **Lithuanian**\n`lb` -> **Luxembourgish**\n`mk` -> **Macedonian**\n`mg` -> **Malagasy**\n`ms` -> **Malay**\n`ml` -> **Malayalam**\n`mt` -> **Maltese**\n`mi` -> **Maori**\n`mr` -> **Marathi**\n`mn` -> **Mongolian**\n`my` -> **Myanmar**\n`ne` -> **Nepali**\n`no` -> **Norwegian**\n`or` -> **Oriya**\n`ps` -> **Pashto**\n`fa` -> **Persian**\n`pl` -> **Polish**\n`pt` -> **Portuguese**\n`pa` -> **Punjabi**\n`ro` -> **Romanian**\n`ru` -> **Russian**\n`sm` -> **Samoan*\n`gd` -> **Scots gaelic**\n`sr` -> **Serbian**\n`st` -> **Sesotho**\n`sn` -> **Shona**\n`sd` -> **Sindhi**\n`si` -> **Sinhala**\n`sk` -> **Slovak**\n`sl` -> **Slovenian**\n`so` -> **Somali**\n`es` -> **Spanish**\n`su` -> **Sundanese**\n`sw` -> **Swahili**\n`sv` -> **Swedish**\n`tg` -> **Tajik**\n`ta` -> **Tamil**\n`tt` -> **Tatar**\n`te` -> **Telugu**\n`th` -> **Thai**\n`tr` -> **Turkish**\n`tk` -> **Turkmen**\n`ug` -> **Uighur**\n`uk` -> **Ukrainian**\n`ur` -> **Urdu**\n`uz` -> **Uzbek**\n`vi` -> **Vietnamese**\n`cy` -> **Welsh**\n`xh -> **Xhosa**\n`yi` -> **Yiddish**\n`yo` -> **Yoruba**\n`zu` -> **Zulu** \n\n⍟───────────⍟",
+        text="⍟────[ʟɪsᴛ]────⍟\n`ʟɪsᴛ ɪs ɪɴ ᴛʜᴇ ғᴏʀᴍ`\n**Language code** => **Language**\n\n`af` ➥ **Afrikaans**\n`sq` -> **Albanian**\n`am` -> **Amharic**\n`ar` -> **Arabic**\n`hy` -> **Armenian**\n`az` -> **Azerbaijani**\n`eu` -> **Basque**\n`be` -> **Belarusian**\n`bn` -> **Bengali**\n`bs` -> **Bosnian**\n`bg` -> **Bulgarian**\n`ca` -> **Catalan**\n`ceb` -> **Cebuano**\n`ny` -> **Chichewa**\n`zh`-`cn` -> **Chinese**\n`co` -> **Corsican**\n`hr` -> **Croatian**\n`cs` -> **Czech**\n`da` -> **Danish**\n`nl` -> **Dutch**\n`en` -> **English**\n`eo` -> **Esperanto**\n`et` -> **Estonian**\n`tl` -> **Filipino**\n`fi` -> **Finnish**\n`fr` -> **French**\n`fy` -> **Frisian**\n`gl` -> **Galician**\n`ka` -> **Georgian**\n`de` -> **German**\n`el` -> **Greek**\n`gu` -> **Gujarati**\n`ht` -> **Haitian creole**\n`ha` -> **Hausa**\n`haw` -> **Hawaiian**\n`iw` -> **Hebrew**\n`hi` -> **Hindi**\n`hmn` -> **Hmong**\n`hu` -> **Hungarian**\n`is` -> **Icelandic**\n`is` -> **Icelandic**\n`ig` -> **Igbo**\n`id` -> **Indonesian**\n`ga` -> **Irish**\n`it` -> **Italian**\n`ja` -> **Japanese*\n`jw` -> **Javanese**\n`kn` -> **Kannada**\n`kk` -> **Kazakh**\n`km` -> **Khmer**\n`rw` -> **Kinyarwanda**\n`ko` -> **Korean**\n`ku` -> **Kurdish (kurmanji)**\n`ky` -> **Kyrgyz**\n`lo` -> **Lao**\n`la` -> **Latin**\n`lv` -> **Latvian**\n`lt` -> **Lithuanian**\n`lb` -> **Luxembourgish**\n`mk` -> **Macedonian**\n`mg` -> **Malagasy**\n`ms` -> **Malay**\n`ml` -> **Malayalam**\n`mt` -> **Maltese**\n`mi` -> **Maori**\n`mr` -> **Marathi**\n`mn` -> **Mongolian**\n`my` -> **Myanmar**\n`ne` -> **Nepali**\n`no` -> **Norwegian**\n`or` -> **Oriya**\n`ps` -> **Pashto**\n`fa` -> **Persian**\n`pl` -> **Polish**\n`pt` -> **Portuguese**\n`pa` -> **Punjabi**\n`ro` -> **Romanian**\n`ru` -> **Russian**\n`sm` -> **Samoan*\n`gd` -> **Scots gaelic**\n`sr` -> **Serbian**\n`st` -> **Sesotho**\n`sn` -> **Shona**\n`sd` -> **Sindhi**\n`si` -> **Sinhala**\n`sk` -> **Slovak**\n`sl` -> **Slovenian**\n`so` -> **Somali**\n`es` -> **Spanish**\n`su` -> **Sundanese**\n`sw` -> **Swahili**\n`sv` -> **Swedish**\n`tg` -> **Tajik**\n`ta` -> **Tamil**\n`tt` -> **Tatar**\n`te` -> **Telugu**\n`th` -> **Thai**\n`tr` -> **Turkish**\n`tk` -> **Turkmen**\n`ug` -> **Uighur**\n`uk` -> **Ukrainian**\n`ur` -> **Urdu**\n`uz` -> **Uzbek**\n`vi` -> **Vietnamese**\n`cy` -> **Welsh**\n`xh -> **Xhosa**\n`yi` -> **Yiddish**\n`yo` -> **Yoruba**\n`zu` -> **Zulu** \n\n⍟───────────⍟",
         reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton("👥 Gʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
+           InlineKeyboardButton("👥 ɢʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
            ]]
            )
        )
@@ -57,9 +57,9 @@ async def list(bot: Client, message: Message):
 @Client.on_message(filters.command("fun"))
 async def fun(bot: Client, message: Message):
     await message.reply_text(
-        text="⍟────Fun────⍟\n🔻**Here is the help for the Fun module**:\n➥ /roll : Roll a dice\n➥ /ball\n➥ /pog\n➥ /throw\n➥ /goal\n➥ /luck\n➥ /run : reply a random string from an array of replie.\n➥ /lnm : find your lucky number.\n ➥ /love : Love 😘\n➥ /toss : Tosses A coin\n➥ /shrug : get shrug XD\n➥ /table : get flip/unflip :v\n➥ /decide : \Randomly answers yes/no/maybe.\n⍟───────────⍟",
+        text="⍟─────[ғᴜɴ]─────⍟\n🔻**Hᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ғᴜɴ ᴍᴏᴅᴜʟᴇ**:\n➥ /roll : Roll a dice\n➥ /ball\n➥ /pog\n➥ /throw\n➥ /goal\n➥ /luck\n➥ /run : reply a random string from an array of replie.\n➥ /lnm : find your lucky number.\n ➥ /love : Love 😘\n➥ /toss : Tosses A coin\n➥ /shrug : get shrug XD\n➥ /table : get flip/unflip :v\n➥ /decide : \Randomly answers yes/no/maybe.\n⍟───────────⍟",
         reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton("👥 Gʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
+           InlineKeyboardButton("👥 ɢʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
            ]]
            )
        )
@@ -69,9 +69,9 @@ async def fun(bot: Client, message: Message):
 @Client.on_message(filters.command("about"))
 async def about(bot: Client, message: Message):
     await message.reply_text(
-        text="╭────[ᴀʙᴏᴜᴛ]────⍟\n├🤖**Mʏ Nᴀᴍᴇ:** [Tg Tool Bot](https://t.me/HTGToolBot)\n├🧑‍💻**Mʏ Dᴇᴠ:** [Hʏᴅʀɪx](https://t.me/Hydrix777)\n├📢**Cʜᴀɴɴᴇʟ:** [TGG](https://t.me/Tg_Galaxy)\n├👥**Gʀᴏᴜᴘ:** [MG](https://t.me/Music_Galaxy_Dl)\n├📡**Sᴇʀᴠᴇʀ:** [Heroku](https://Heroku.com)\n├🔣**Language:** [Python](https://python.org/)\n╰───────────⍟",
+        text="╭────[ᴀʙᴏᴜᴛ]────⍟\n├🤖**ᴍʏ ɴᴀᴍᴇ:** [ʜʏᴅʀɪx ᴛᴏᴏʟ ʙᴏᴛ](https://t.me/HTGToolBot)\n├🧑‍💻**ᴍʏ ᴅᴇᴠ:** [Hʏᴅʀɪx](https://t.me/Hydrix777)\n├📢**ᴄʜᴀɴɴᴇʟ:** [ᴛɢɢ](https://t.me/Tg_Galaxy)\n├👥**ɢʀᴏᴜᴘ:** [ᴍɢ](https://t.me/Music_Galaxy_Dl)\n├📡**sᴇʀᴠᴇʀ:** [ʜᴇʀᴏᴋᴜ](https://Heroku.com)\n├🔣**ʟᴀɴɢᴜᴀɢᴇ:** [ᴘʏᴛʜᴏɴ](https://python.org/)\n╰───────────⍟",
         reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton("👥 Gʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
+           InlineKeyboardButton("👥 ɢʀᴏᴜᴘ", url="https://t.me/Music_Galaxy_Dl")
            ]]
            )
        )
@@ -80,7 +80,7 @@ async def about(bot: Client, message: Message):
 
 @Client.on_message(filters.private & filters.sticker)
 async def stickers(_, message):
-       await message.reply(f"Your Requested Sticker's ID is👇\n\n* `{message.sticker.file_id}` *", quote=True)
+       await message.reply(f"ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛᴇᴅ sᴛɪᴄᴋᴇʀ's ɪᴅ ɪs👇\n\n* `{message.sticker.file_id}` *", quote=True)
 
 
 # Service clear
@@ -112,7 +112,7 @@ async def getimage(client, message):
         os.makedirs(location)
     imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
     dwn = await client.send_message(
-          text="<b>𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>",
+          text="<b>ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...</b>",
           chat_id = message.chat.id,
           reply_to_message_id=message.message_id
           )          
@@ -120,11 +120,11 @@ async def getimage(client, message):
             message=message,
             file_name=imgdir
         )
-    await dwn.edit_text("<b>𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>")
+    await dwn.edit_text("<b>ᴜᴘʟᴏᴀᴅɪɴɢ...</b>")
     try:
         response = upload_file(imgdir)
     except Exception as error:
-        await dwn.edit_text(f"𝗢𝗼𝗽𝘀 𝗦𝗼𝗺𝗲𝘁𝗵𝗶𝗻𝗴 𝗪𝗲𝗻𝘁 𝗪𝗿𝗼𝗻𝗴\n{error} Contact @HydraLivegrambot")
+        await dwn.edit_text(f"Oᴏᴘs sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ\n{error} ᴄᴏɴᴛᴀᴄᴛ @HydraLivegrambot")
         return
     await dwn.edit_text(f"https://telegra.ph{response[0]}")
     try:
