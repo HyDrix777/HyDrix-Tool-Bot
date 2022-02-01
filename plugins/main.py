05b2ff10d02e491f2c37749da8e1feeea33460d0
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-01 12:51:33 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -89,14 +89,6 @@ async def stickers(_, message):
 async def delete(bot,message):
  await message.delete()
 
-@Client.on_message(filters.left_chat_member)
-async def goodbye(bot, message):
- await message.delete()
-
-@Client.on_message(filters.new_chat_members)
-async def welcome(bot, message):
- await message.delete() 
-
 # Telegraph
 
 @Client.on_message(filters.private & filters.photo)
