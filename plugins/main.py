dfcec6918eca51e95d2598a423b79d208c462f76
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-01 13:06:30 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -85,7 +85,23 @@ async def stickers(_, message):
 
 # Service clear
 
-
+@Client.on_message(filters.group & filters.service)
+async def delete_events(bot, msg):
+    try:
+        chat_member = await bot.get_chat_member(msg.chat.id, "self")
+    except Exception:  # Like kicked, etc.
+        return
+    if chat_member.status == "administrator":
+        if chat_member.can_delete_messages:
+            await msg.delete()
+        else:
+            await msg.reply(
+                "I'm surely admin, but I don't have 'Delete Messages' permission !!!"
+            )
+    else:
+        await msg.reply(
+            "I'm not admin, so I can't delete messages. \n\nMake me admin or kick me outta here."
+        )
 
 # Telegraph
 
