b7c34615bc257d2723eefab9bb7276e6694bbe2f
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-01 16:03:29 +0530

    Update animation.py

--- plugins/animation.py
+++ plugins/animation.py
@@ -1,9 +1,6 @@
 import time
 from typing import List
-
-from telegram import Update
 from telegram.ext import run_async, CallbackContext
-
 from SiestaRobot import dispatcher
 from SiestaRobot.modules.disable import DisableAbleCommandHandler
 from SiestaRobot.modules.helper_funcs.chat_status import user_admin
