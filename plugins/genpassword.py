f620ebc8d3de22eb2aa6d569310aaf48795e479a
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-03 01:11:54 +0530

    Create genpassword.py

---
+++ plugins/genpassword.py
@@ -0,0 +1,21 @@
+import random, os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["genpass", 'genpw']))
+async def password(bot, update):
+    message = await update.reply_text(text="`Processing...`")
+    password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
+    try:
+        limit = int(message.text)
+    except:
+        await message.edit_text('Limit is wrong')
+        return
+    if limit > 100 or limit <= 0:
+        text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
+    else:
+        random_value = "".join(random.sample(password, limit))
+        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @Movie_flix7",
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Movie channel', url='https://t.me/Movie_flix7')]])
+    await message.edit_text(text, True)
