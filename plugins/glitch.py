f1a0dedc3d8ac15108c208fd12cc47999c5ec15b
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-02 10:56:25 +0530

    Create glitch.py

---
+++ plugins/glitch.py
@@ -0,0 +1,48 @@
+import os
+import glitchart
+from pyrogram import Client, filters
+
+
+
+PATH = os.environ.get("PATH", "./DOWNLOADS")
+
+
+
+@Bot.on_message(filters.private("glitch"))
+async def glitch(bot, update):
+    download_path = PATH + "/" + str(update.from_user.id) + "/"
+    download_location = download_path + "photo.jpg"
+    message = await update.reply_text(
+        text="`Processing...`",
+        disable_web_page_preview=True,
+        quote=True
+    )
+    try:
+        await update.download(
+            file_name=download_location
+        )
+    except Exception as error:
+        await message.edit_text(
+            text=f"**Error :** `{error}`\nContact @TheFayas.",
+            disable_web_page_preview=True
+        )
+        return 
+    await message.edit_text(
+        text="`Converting to glitch art...`"
+    )
+    try:
+        glitch_art = glitchart.jpeg(download_location)
+        await update.reply_photo(
+            photo=glitch_art,
+            caption=update.caption,
+            quote=True
+        )
+        os.remove(download_location)
+        os.remove(glitch_art)
+    except Exception as error:
+        await message.edit_text(
+            text=f"**Error :** `{error}`\nContact @TheFayas.",
+            disable_web_page_preview=True
+        )
+        return
+    await message.delete()
