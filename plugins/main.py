7ae6cc3bfa6d70de5a9a08e1dee01d0773119bf5
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-02 10:48:22 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -9,6 +9,7 @@ from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, Peer
 from telegraph import upload_file
 import requests 
 import lyricsgenius
+import telegraminfo
 
 
 
@@ -158,3 +159,27 @@ async def lrsearch(_, message: Message):
 **__Lyrics:__**
 {S.lyrics}"""
     await m.edit(xxx)
+
+# Info--------
+
+@Bot.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
+async def info(bot, update):
+    if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
+        info = telegraminfo.user(update.from_user)
+    elif update.reply_to_message and update.reply_to_message.forward_from:
+        info = telegraminfo.user(update.reply_to_message.forward_from)
+    elif update.reply_to_message and update.reply_to_message.forward_from_chat:
+        info = telegraminfo.chat(update.reply_to_message.forward_from_chat)
+    elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
+        info = telegraminfo.user(update.reply_to_message.from_user)
+    else:
+        return
+    try:
+        await update.reply_text(
+            text=info+"\n\nMade by @HTGToolBot",
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
