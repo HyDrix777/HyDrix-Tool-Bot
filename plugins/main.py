fcfcd276fe870f6ed35598dca06a74e575a99922
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-03 16:22:23 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -93,6 +93,21 @@ async def delete(bot,message):
 async def goodbye(bot, message):
  await message.delete()
 
+# PIN UNPIN-------
+
+@Client.on_message(filters.command(["pin"], COMMAND_HAND_LER) & admin_fliter)
+async def pin(_, message: Import.Msg):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.pin()
+
+@Client.on_message(filters.command(["unpin"], COMMAND_HAND_LER) & admin_fliter)
+async def unpin(_, message: Import.Msg):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.unpin()
+
+
 # Telegraph
 
 @Client.on_message(filters.private & filters.photo)
