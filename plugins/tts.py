d0e08264861c62c491a3e97f172f07f1929ace3d
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-03 23:21:16 +0530

    Update tts.py

--- plugins/tts.py
+++ plugins/tts.py
@@ -19,7 +19,7 @@ async def is_register_admin(chat, user):
         return True
 
 
-@register(pattern="^/tts (.*)")
+@Client(pattern="^/tts (.*)")
 async def _(event):
     if event.fwd_from:
         return
