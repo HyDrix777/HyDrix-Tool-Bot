61b94721df02b8a9db607e24e0a285208c178014
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-07 21:37:36 +0530

    Update autoapprove.py

--- plugins/autoapprove.py
+++ plugins/autoapprove.py
@@ -8,7 +8,8 @@ CHAT_ID=int(os.environ.get("CHAT_ID", None))
 TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
 
-    await message.reply_text(text="**__Hello Iam Auto Approved Join Request Bot**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+    await message.reply_text(text="**Hello Iam Auto Approved Join Request Bot**",
+        reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
 @Client.on_chat_join_request(filters.chat(CHAT_ID))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
