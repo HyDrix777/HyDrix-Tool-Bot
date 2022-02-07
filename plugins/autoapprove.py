effcb8cb49a33cb607684f084a6da6a4a4e5e901
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-07 21:26:05 +0530

    Create autoapprove.py

---
+++ plugins/autoapprove.py
@@ -0,0 +1,23 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
+
+
+
+
+CHAT_ID=int(os.environ.get("CHAT_ID", None))
+TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
+
+
+@Client.on_chat_join_request(filters.chat(CHAT_ID))
+async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} Joined 🤝") # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        print("Welcome....")
+
+print("Auto Approved Bot")
