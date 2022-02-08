da35a6595ce50cf764d37a1bbb5bf37167c31798
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-08 12:57:47 +0530

    Create linkshorter.py

---
+++ plugins/linkshorter.py
@@ -0,0 +1,26 @@
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+
+    
+
+
+@Client.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'❗ Here is your [`{short_link}`]({short_link}) \n\n 〽️ Powered by @GroupDcBots', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://shortzon.com/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
