d01efecfc7cc2ffb5d66bf8c5df23e532342148d
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-08 17:29:05 +0530

    Create pinunpin.py

---
+++ plugins/pinunpin.py
@@ -0,0 +1,16 @@
+from pyrogram import filters, Client as DonLee_Robot_V2
+from DonLee_Robot_V2 import Config, Import
+COMMAND_HAND_LER = Config.COMMAND_HAND_LER
+from DonLee_Robot_V2.Admins import admin_check, extract_user, admin_fliter
+
+@Client.on_message(filters.command(["pin"], COMMAND_HAND_LER) & admin_fliter)
+async def pin(_, message: Import.Msg):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.pin()
+
+@Client.on_message(filters.command(["unpin"], COMMAND_HAND_LER) & admin_fliter)
+async def unpin(_, message: Import.Msg):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.unpin()
