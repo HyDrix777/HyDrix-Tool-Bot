07aad31d576464698a915125b16ce85b747c0a98
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-08 16:42:30 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -145,3 +145,11 @@ async def lrsearch(_, message: Message):
 **__Lyrics:__**
 {S.lyrics}"""
     await m.edit(xxx)
+
+
+@Client.on_message(filters.command(["stickerid"]))
+async def stickerid(bot, message):   
+    if message.reply_to_message.sticker:
+       await message.reply(f"Sticker ID is \n <code>{message.reply_to_message.sticker.file_id}</code>\n\nUnique ID is\n\n<code>{message.reply_to_message.sticker.file_unique_id}</code>", reply_markup=BUTTONS, quote=True)
+    else: 
+       await message.reply("Oops !! Not a sticker file")
