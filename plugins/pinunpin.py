3b539049214a7397a70dd02adb4e5a5f3ca777c1
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-08 17:37:29 +0530

    Update pinunpin.py

--- plugins/pinunpin.py
+++ plugins/pinunpin.py
@@ -1,13 +1,13 @@
 from pyrogram import filters, Client
 from Client.Admins import admin_check, extract_user, admin_fliter
 
-@Client.on_message(filters.command(["pin"], COMMAND_HAND_LER) & admin_fliter)
+@Client.on_message(filters.command(["pin"]) & admin_fliter)
 async def pin(_, message: Import.Msg):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
-@Client.on_message(filters.command(["unpin"], COMMAND_HAND_LER) & admin_fliter)
+@Client.on_message(filters.command(["unpin"]) & admin_fliter)
 async def unpin(_, message: Import.Msg):
     if not message.reply_to_message:
         return
