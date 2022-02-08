e830f6eaa22658e5ebb1d79a09fce1b03583860f
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-08 16:47:25 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -150,6 +150,6 @@ async def lrsearch(_, message: Message):
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"Sticker ID is \n <code>{message.reply_to_message.sticker.file_id}</code>\n\nUnique ID is\n\n<code>{message.reply_to_message.sticker.file_unique_id}</code>", reply_markup=BUTTONS, quote=True)
+       await message.reply(f"Sticker ID is \n <code>{message.reply_to_message.sticker.file_id}</code>\n\nUnique ID is\n\n<code>{message.reply_to_message.sticker.file_unique_id}</code>")
     else: 
        await message.reply("Oops !! Not a sticker file")
