47a893e27d8c4d4069d1304ca41f0694ee34412c
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-10 22:07:14 +0530

    Create channelban.py

---
+++ plugins/channelban.py
@@ -0,0 +1,19 @@
+from config import Config
+from pyrogram import Client, idle
+from pyrogram.raw.types import UpdateGroupCallParticipants, PeerChannel
+from pyrogram.raw.functions.channels import EditBanned
+from pyrogram.raw.types import InputPeerChannel, InputChannel, ChatBannedRights, InputGroupCall
+from pyrogram.raw.functions.phone import EditGroupCallParticipant, GetGroupParticipants
+
+
+
+@Client.on_raw_update()
+async def hemheupdet(client, update, users, chats):
+    banchat = await client.resolve_peer("DecodeSupport")
+    if isinstance(update, UpdateGroupCallParticipants):
+        for x in update.participants:
+            if isinstance(x.peer, PeerChannel):
+                    await client.send(EditGroupCallParticipant(call=update.call, participant=x.peer (int(str(-100) + str(x.peer.channel_id))), muted=True))
+                    await client.kick_chat_member("DecodeSupport", x.peer.channel_id)
+                    await client.send_message(chat, f"Successfully Banned str(x.peer.channel_id)")
+                    await client.send_message(chat, f"{e} \n\n{x.peer}")
