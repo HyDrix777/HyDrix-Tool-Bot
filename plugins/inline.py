4a436c27c4ca96411ca18bd7a42cea11402db0bf
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-10 23:45:30 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,32 +1,51 @@
-from pyrogram import Client ,filters
-import os
-from py_youtube import Data, Search 
-from pyrogram.types import *
+from pyrogram import Client, errors
+from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 
-	
+from youtubesearchpython import VideosSearch
 
 
 @Client.on_inline_query()
-async def search_video(client,query):
-	search = []
-	result = Search(query.query.strip()).videos()
-	for i in result:
-		try:
-			title = i["title"]
-			id = i["id"]
-			thumb = i["thumb"][0]
-			data = i["simple_data"]
-		except:
-			pass
-		try:
-			search.append(
-                InlineQueryResultPhoto(
-                    title=title,
-                    description=data,
-                    caption="https://youtu.be/"+id,
-                    photo_url=thumb))
-		
-		except:
-		          pass
-            
-	await query.answer(search)
+async def inline(client: Client, query: InlineQuery):
+    answers = []
+    search_query = query.query.lower().strip().rstrip()
+
+    if search_query == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text="Type a YouTube video name...",
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+    else:
+        search = VideosSearch(search_query, limit=50)
+
+        for result in search.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=result["title"],
+                    description="{}, {} views.".format(
+                        result["duration"],
+                        result["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "https://www.youtube.com/watch?v={}".format(
+                            result["id"]
+                        )
+                    ),
+                    thumb_url=result["thumbnails"][0]["url"]
+                )
+            )
+
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text="Error: Search timed out",
+                switch_pm_parameter="",
+            )
