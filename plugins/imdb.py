7c5c72ffebd3d2ffeceb2b9682a1bf6bc350fb82
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-10 13:17:16 +0530

    Update imdb.py

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -7,8 +7,8 @@ import logging
 
 
 
-
-
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 Title:</b> <a href={url}>{title}</a> [{year}]\n<b>📺 Type:</b> {kind}\n<b>📆 Release:</b> <a href={url}/releaseinfo>{release_date}</a>\n<b>🌟 Rating:</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>📀 Runtime:</b> <code>{runtime} minutes</code>\n<b>🎭 Genres:</b> {genres}\n\n<b>☀️ Languages:</b> {languages}\n<b>🎛 Countries:</b> {countries}\n<b>🎥 Director:</b> {director}\n<b>📝 Writers:</b> {writer}\n\n<b><a href='https://t.me/+q9NMdy0rY10zZWZl'>© IMDb (Series & Movie) Studio</a></b>\n\n<b>✍️ Note:</b> <s>This message will be Auto-deleted after 10 minutes to avoid copyright issues.</s>")
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
 
 
 
@@ -53,7 +53,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
         ]
     message = quer_y.message.reply_to_message or quer_y.message
     if imdb:
-        caption = format(
+        caption = IMDB_TEMPLATE.format(
             title = imdb['title'],
             votes = imdb['votes'],
             aka = imdb["aka"],
