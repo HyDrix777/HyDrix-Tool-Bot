c652b75d195c9e46660e1b91906fe1374e73f5d7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-10 20:47:23 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 
 
 @Client.on_message(filters.command("carbon"))
-@capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
         return await message.reply_text(
