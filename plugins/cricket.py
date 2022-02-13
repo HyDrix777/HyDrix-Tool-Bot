da10d990290ccce513df6c68f75134d1204b3498
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-14 00:53:13 +0530

    Update cricket.py

--- plugins/cricket.py
+++ plugins/cricket.py
@@ -2,7 +2,6 @@ import urllib.request
 from pyrogram import Client
 from bs4 import BeautifulSoup
 from telethon import events
-from NoinoiRobot import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
