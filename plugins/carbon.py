ff41702d55d523dd7ee33cd3dd071e24144886df
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-14 12:09:46 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -1,7 +1,4 @@
 from platform import python_version as y
-from telegram import __version__ as o
-from pyrogram import __version__ as z
-from telethon import __version__ as s
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
 from plugins import pbot
