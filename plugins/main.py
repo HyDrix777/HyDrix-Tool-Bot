32c499211460cde068fb2c2c725c0afc353bef2c
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-15 10:52:27 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -295,10 +295,7 @@ async def list(bot: Client, message: Message):
 
 # Sticker id-----------------------
 
-@Client.on_message(filters.stiker("sticker"))
-async def stickers(_, message):
-    if msg.reply_to_message.sticker:
-       await message.reply(f"Your Requested Sticker's ID is👇\n\n* `{message.sticker.file_id}` *", quote=True)
+
 
 
 # Service clear--------------------
