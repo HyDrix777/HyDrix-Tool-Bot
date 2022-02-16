c2be80326544ca1c257e99d39b54b48eaec97278
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-16 18:41:54 +0530

    Update calculator.py

--- plugins/calculator.py
+++ plugins/calculator.py
@@ -46,18 +46,6 @@ CALCULATE_BUTTONS = InlineKeyboardMarkup(
     )
 
 
-@Client.on_message(filters.command(["start"]))
-async def start(bot, update):
-    text = START_TEXT.format(update.from_user.mention)
-    reply_markup = START_BUTTONS
-    await update.reply_text(
-        text=text,
-        disable_web_page_preview=True,
-        reply_markup=reply_markup,
-        quote=True
-    )
-
-
 @Client.on_message(filters.private & filters.command(["calc", "calculate", "calculator"]))
 async def calculate(bot, update):
     await update.reply_text(
