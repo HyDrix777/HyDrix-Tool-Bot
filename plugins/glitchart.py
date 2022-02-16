d3464c1358024dd9da3ce95a232640c60f4be231
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-16 23:52:22 +0530

    Update glitchart.py

--- plugins/glitchart.py
+++ plugins/glitchart.py
@@ -25,11 +25,11 @@ async def glitch(bot, update):
             file_name=download_location
         )
     except Exception as error:
-        await message.edit_photo(
+        await update.edit_photo(
             text=f"Error : {error}\nContact @rdgjhalbin_praveen."
         )
         return 
-    await message.edit_photo(
+    await update.edit_photo(
         text="Converting to glitch..."
     )
     try:
