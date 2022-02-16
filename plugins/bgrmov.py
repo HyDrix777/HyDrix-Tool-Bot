472223859101df23d553ff6cf040cb6478da5519
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-16 21:51:51 +0530

    Update bgrmov.py

--- plugins/bgrmov.py
+++ plugins/bgrmov.py
@@ -21,7 +21,7 @@ ERROR_BUTTONS = InlineKeyboardMarkup(
 
 
 
-@Bot.on_message(filters.private & (filters.photo | filters.video | filters.document))
+@Client.on_message(filters.private & (filters.photo | filters.video | filters.document))
 async def remove_background(bot, update):
     if not REMOVEBG_API:
         await update.reply_text(
