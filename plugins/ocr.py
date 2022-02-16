a513a5e7a91811f7bdb6cd6b2f918c8aa04a4973
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-16 21:29:00 +0530

    Update ocr.py

--- plugins/ocr.py
+++ plugins/ocr.py
@@ -1,6 +1,5 @@
 
 from telegram import ChatAction,InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackQueryHandler,PicklePersistence
 import logging
 import os
