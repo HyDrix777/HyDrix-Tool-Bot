85ddbe74301b33f852107f4e3d207244c2c54db7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-16 13:19:02 +0530

    Create pinunpin.py

---
+++ plugins/pinunpin.py
@@ -0,0 +1,37 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+
+@Client.on_message(
+    filters.command(["pin"], /) &
+    async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
+
+async def pin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.pin()
+
+
+@Client.on_message(
+    filters.command(["unpin"], /) &
+    async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
+
+async def unpin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.unpin()
