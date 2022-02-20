aceb6a81e1c071afdfbfe41ce6695ef19d16a67d
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-20 22:04:32 +0530

    Create cmd.py

---
+++ plugins/cmd.py
@@ -0,0 +1,55 @@
+import asyncio
+import shlex
+import time
+from typing import Tuple
+from pyrogram.types import Message
+from pyrogram import Client
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def fetch_audio(client, message):
+    time.time()
+    if not message.reply_to_message:
+        await message.reply("**Reply Video or Audio**")
+        return
+    warner_stark = message.reply_to_message
+    if warner_stark.audio is None and warner_stark.video is None:
+        await message.reply("**Format not Supported**")
+        return
+    if warner_stark.video:
+        rsr2 = await message.reply("**Converting😁**")
+        warner_bros = await message.reply_to_message.download()
+        stark_cmd = f"ffmpeg -i {warner_bros} -map 0:a friday.mp3"
+        await runcmd(stark_cmd)
+        final_warner = "friday.mp3"
+    elif warner_stark.audio:
+        rsr2 = await edit_or_reply(message, "**Downloading🤭**")
+        final_warner = await message.reply_to_message.download()
+    await rsr2.edit("**Finished😊**")
+    await rsr2.delete()
+    return final_warner
+
+
+async def edit_or_reply(message, text, parse_mode="md"):
+    if message.from_user.id:
+        if message.reply_to_message:
+            kk = message.reply_to_message.message_id
+            return await message.reply_text(
+                text, reply_to_message_id=kk, parse_mode=parse_mode
+            )
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
