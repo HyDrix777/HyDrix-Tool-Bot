e83bd24e4e3215319c30e14793a87ede66ff9423
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-21 14:41:03 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,4 +1,5 @@
 import os
+from pyrogram import Client
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
