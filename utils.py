d3a664f39b61a8a68a68499efcdab8567c7e9621
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-22 16:41:06 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,6 +14,58 @@ from datetime import datetime
 
 imdb = IMDb()
 
+async def get_poster(query, bulk=False, id=False):
+    if not id:
+        # https://t.me/GetTGLink/4183
+        pattern = re.compile(r"^(([a-zA-Z\s])*)?\s?([1-2]\d\d\d)?", re.IGNORECASE)
+        match = pattern.match(query)
+        year = None
+        if match:
+            title = match.group(1)
+            year = match.group(3)
+        else:
+            title = query
+        movieid = imdb.search_movie(title.lower(), results=10)
+        if not movieid:
+            return None
+        if year:
+            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
+            if not filtered:
+                filtered = movieid
+        else:
+            filtered = movieid
+        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
+        if not movieid:
+            movieid = filtered
+        if bulk:
+            return movieid
+        movieid = movieid[0].movieID
+    else:
+        movieid = int(query)
+    movie = imdb.get_movie(movieid)
+    title = movie.get('title')
+    genres = ", ".join(movie.get("genres")) if movie.get("genres") else None
+    rating = str(movie.get("rating"))
+    if movie.get("original air date"):
+        date = movie["original air date"]
+    elif movie.get("year"):
+        date = movie.get("year")
+    else:
+        date = "N/A"
+    poster = movie.get('full-size cover url')
+    plot = movie.get('plot outline')
+    if plot and len(plot) > 800:
+        plot = plot[0:800] + "..."
+    return {
+        'title': title,
+        'year': date,
+        'genres': genres,
+        'poster': poster,
+        'plot': plot,
+        'rating': rating,
+        'url':f'https://www.imdb.com/title/tt{movieid}'
+
+    }
 
 # Broadcast-----------------------
 
@@ -38,8 +90,6 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
-
-#
 
 def extract_user(message: Message) -> Union[int, str]:
     """extracts the user from a message"""
@@ -103,60 +153,6 @@ def get_file_id(msg: Message):
             if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
-
-
-async def get_poster(query, bulk=False, id=False):
-    if not id:
-        # https://t.me/GetTGLink/4183
-        pattern = re.compile(r"^(([a-zA-Z\s])*)?\s?([1-2]\d\d\d)?", re.IGNORECASE)
-        match = pattern.match(query)
-        year = None
-        if match:
-            title = match.group(1)
-            year = match.group(3)
-        else:
-            title = query
-        movieid = imdb.search_movie(title.lower(), results=10)
-        if not movieid:
-            return None
-        if year:
-            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
-            if not filtered:
-                filtered = movieid
-        else:
-            filtered = movieid
-        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
-        if not movieid:
-            movieid = filtered
-        if bulk:
-            return movieid
-        movieid = movieid[0].movieID
-    else:
-        movieid = int(query)
-    movie = imdb.get_movie(movieid)
-    title = movie.get('title')
-    genres = ", ".join(movie.get("genres")) if movie.get("genres") else None
-    rating = str(movie.get("rating"))
-    if movie.get("original air date"):
-        date = movie["original air date"]
-    elif movie.get("year"):
-        date = movie.get("year")
-    else:
-        date = "N/A"
-    poster = movie.get('full-size cover url')
-    plot = movie.get('plot outline')
-    if plot and len(plot) > 800:
-        plot = plot[0:800] + "..."
-    return {
-        'title': title,
-        'year': date,
-        'genres': genres,
-        'poster': poster,
-        'plot': plot,
-        'rating': rating,
-        'url':f'https://www.imdb.com/title/tt{movieid}'
-
-    }
 
 def last_online(from_user):
     time = ""
