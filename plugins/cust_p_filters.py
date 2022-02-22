dca312c082b9c88769c056947f89a05482f54386
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 01:33:47 +0530

    Update cust_p_filters.py

--- plugins/cust_p_filters.py
+++ plugins/cust_p_filters.py
@@ -5,6 +5,25 @@ from plugins.admin_check import admin_check
 import os
 
 
+USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
+
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in ADMINS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
 
 
 
