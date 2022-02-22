10be7b6ac4c7cbd858d68f49dca9b24505391766
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-22 14:35:26 +0530

    Create infog.py

---
+++ infog.py
@@ -0,0 +1,9 @@
+import re
+from os import environ
+id_pattern = re.compile(r'^.\d+$')
+
+
+
+
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
