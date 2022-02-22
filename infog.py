f4923c8ab2b1ac1fe43279234ed1b7a7dfc03fbc
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 01:23:43 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -8,3 +8,4 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+TG_MAX_SELECT_LEN = 100
