ed42f1672938d1b88fe5f2181af016cd5ef6e3fd
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-22 12:30:39 +0530

    Create github.py

--- plugins/github.py
+++ plugins/github.py
@@ -31,16 +31,16 @@ async def github(_, message):
                 followers = result["followers"]
                 following = result["following"]
                 caption = f"""**Info Of {name}**
-**Username:** `{username}`
-**Bio:** `{bio}`
-**Profile Link:** [Here]({url})
-**Company:** `{company}`
-**Created On:** `{created_at}`
-**Repositories:** `{repositories}`
-**Blog:** `{blog}`
-**Location:** `{location}`
-**Followers:** `{followers}`
-**Following:** `{following}`"""
+**👨‍💼 Username:** `{username}`
+**✍️ Bio:** `{bio}`
+**🔗 Profile Link:** [Here]({url})
+**🏢 Company:** `{company}`
+**🔨 Created On:** `{created_at}`
+**⛵️ Repositories:** `{repositories}`
+**🧖 Blog:** `{blog}`
+**📍 Location:** `{location}`
+**➡️ Followers:** `{followers}`
+**⬅️ Following:** `{following}`"""
             except Exception as e:
                 print(str(e))
     await message.reply_photo(photo=avatar_url, caption=caption)
