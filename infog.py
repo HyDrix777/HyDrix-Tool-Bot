5ad828c9b6e7a029f1b056420eda623b3a824ed4
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 21:00:17 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -1,8 +1,6 @@
 import re
 from os import environ
 id_pattern = re.compile(r'^.\d+$')
-import os
-import logging
 
 
 
@@ -11,5 +9,3 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Tg_galaxy")
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
