c776a20f5f89980928ad48f8665461c6c893864a
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 17:19:38 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -1,6 +1,7 @@
 import re
 from os import environ
 id_pattern = re.compile(r'^.\d+$')
+import os
 
 
 
@@ -10,3 +11,5 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
 
+sudo_users = int(os.getenv("sudo_users"))
+owner_id = int(os.getenv("owner_id"))
