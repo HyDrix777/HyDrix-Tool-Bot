6a165bb60a2dcda8f15550aae9ecafc628883d7f
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 19:30:07 +0530

    Update whisper.py

--- plugins/whisper.py
+++ plugins/whisper.py
@@ -2,7 +2,7 @@ from telethon import events, TelegramClient, Button
 import logging
 from telethon.tl.functions.users import GetFullUserRequest as us
 import os
-
+from pyrogram import Client, filters
 
 logging.basicConfig(level=logging.INFO)
 
