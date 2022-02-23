f79980bfb47eadc96270cf137dc9de7926df137a
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 17:15:00 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -1,7 +1,7 @@
 import re
 from os import environ
 id_pattern = re.compile(r'^.\d+$')
-import os
+
 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '784589736').split()]
@@ -10,13 +10,3 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
 
-START_TXT = """Hey {}, Iam a Guess Game Bot 
-
-Hope You Enjoyed The Bot
-"""
-
-GUESS_COUNT = int(environ.get("GUESS_COUNT", "14"))
-
-MAX_GUESSES = int(environ.get("MAX_GUESSES", "20"))
-
-SECRET_WORD = environ.get("Any word that you want your users to guess")
