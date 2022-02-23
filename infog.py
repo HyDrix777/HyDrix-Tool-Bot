8a48ee3e469ea53b84ee5635da5b0763827e81d8
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 20:54:06 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -11,25 +11,5 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S'
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-class Config(object):
-    # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-    # Group / channel username of the support chat
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Tg_galaxy")
-
-    # List of admin user ids for special functions(Storing as an array)
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
-
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Tg_galaxy")
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
