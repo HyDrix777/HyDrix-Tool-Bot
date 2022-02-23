89c4267cd621f0e31bcb35f0aecb81581119d069
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 12:02:02 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -10,6 +10,11 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
 
+
+
 class Config(object):
-    MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
     SESSION_NAME = os.environ.get("Torrent-Search-Bot")
+    API_ID = int(os.environ.get("API_ID", 18891187)
+    API_HASH = os.environ.get("7d120384f48b2a86fa2b9e9772a28af6")
+    BOT_TOKEN = os.environ.get("5219376297:AAFydXWjPg47TlZ4QnVkc2egji4mPMq0_2w")
+    MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
