ad6d0692e97615dfb595f1d8343721def9b13c44
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 20:05:19 +0530

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,5 @@
+class Presets(object):
+   
+    USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
+    WAIT_MSG = "<b>Please Wait...</b>"
+    REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
