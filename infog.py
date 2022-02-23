9a7d7e9935a4a3c8a1b7eee37ddb47ada3bb965a
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 11:44:56 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -9,5 +9,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
-MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
-SESSION_NAME = os.environ.get("Torrent-Search-Bot")
+
+class Config(object):
+    MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
+    SESSION_NAME = os.environ.get("Torrent-Search-Bot")
