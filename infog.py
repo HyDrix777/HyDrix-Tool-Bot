4bec9d00c3a0e73e6f0bb21bfb48fd3ef65dd7a7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-23 16:57:16 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -8,5 +8,3 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 Title:</b> <a href={url}>{title}</a>\n<b>📺 Type:</b> {kind}\n<b>📆 Release:</b> <a href={url}/releaseinfo>{release_date}</a>\n<b>🌟 Rating:</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>📀 Runtime:</b> <code>{runtime} minutes</code>\n<b>🎭 Genres:</b> {genres}\n\n<b>☀️ Languages:</b> {languages}\n<b>🎛 Countries:</b> {countries}\n<b>🎥 Director:</b> {director}\n<b>📝 Writers:</b> {writer}\n\n<b><a href='https://t.me/+q9NMdy0rY10zZWZl'>© IMDb (Series & Movies) Studio</a></b>\n\n<b>✍️ Note:</b> <s>This message will be Auto-deleted after 10 minutes to avoid copyright issues.</s>")
-
