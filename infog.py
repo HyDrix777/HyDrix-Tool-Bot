55a4dc73fb332108ccc1c6fc6ca256f40134d2bd
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-24 17:30:52 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -5,6 +5,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 
 
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001568108953'))
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '784589736').split()]
