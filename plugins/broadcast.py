2b466dadd2d2e45e6220ff61b415ecc75f5e8030
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-24 17:18:57 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 import datetime
 import time
 from plugins.users_chats_db import db
-from infog import ADMINS
+from info import ADMINS
 from utils import broadcast_messages
 import asyncio
         
