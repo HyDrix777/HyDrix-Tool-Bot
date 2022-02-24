733b0d9df8eaa976fa25bc8556db918a6d0f9e78
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-24 17:24:49 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -7,5 +7,6 @@ id_pattern = re.compile(r'^.\d+$')
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '784589736').split()]
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
