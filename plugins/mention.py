e72200cd7916f0bc0a3c963c27477fd13b783276
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-25 20:59:20 +0530

    Update mention.py

--- plugins/mention.py
+++ plugins/mention.py
@@ -3,7 +3,7 @@ from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
-from telethon import Client
+from bot import Client
 
 logging.basicConfig(
     level=logging.INFO,
