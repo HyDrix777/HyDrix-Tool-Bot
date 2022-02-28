b0a25bec69711da9e4fa686d874e643378ef7e83
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-28 22:55:29 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -12,4 +12,5 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '784589736').split()]
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = 100
-LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", True)) 
+LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", True))
+IMDB = bool((environ.get('IMDB', True)))
