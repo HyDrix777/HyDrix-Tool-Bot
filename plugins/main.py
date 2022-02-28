c2b09c8c53b79ccbd02ef42e1e03d9f8890be1ea
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-02-28 11:22:54 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -444,7 +444,7 @@ async def list(bot: Client, message: Message):
 # Kick----
 
 @Client.on_message(filters.group & filters.command("ban"))
-def ban(bot, message):
+async def ban(bot, message):
     bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
     bot.send_message(message.chat.id, f"{message.reply_to_message.from_user_mention} Banned!")
 
