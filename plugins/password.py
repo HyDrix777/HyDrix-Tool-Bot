8580dceb1fe92afdbc2755086a80c2d8a9eec685
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-01 21:47:59 +0530

    Create password.py

---
+++ plugins/password.py
@@ -0,0 +1,25 @@
+import random, os
+from pyrogram import Client, filters
+
+
+
+
+@Bot.on_message(filters.private & filters.command("password"))
+async def password(bot, update):
+    
+    message = await message.reply_text('`Processing...`')
+    password = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"!@#$%^&*()_+".lower()
+    
+    try:
+        limit = int(message.text)
+    except:
+        await message.edit_text('Limit is wrong')
+        return
+    
+    if limit > 100 or limit <= 0:
+        text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
+    else:
+        random_value = "".join(random.sample(password, limit))
+        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @EKBOTZ_UPDATE"
+    
+    await message.edit_text(text, True)
