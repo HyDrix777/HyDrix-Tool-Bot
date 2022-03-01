3222a1baf0019928e4e498af0887b81f91a9f79e
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-01 16:13:04 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+class Config(object):
+
+    
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+
+    
+    DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "Cluster0"))
+
+
+    # ID of users that can use the bot commands
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+
+    SAVE_USER = os.environ.get("SAVE_USER", "no").lower()
+
+
+    # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
+    # To check dyno status
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "None")
+
+
+    # OPTIONAL - To set alternate BOT COMMANDS
+    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
+    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
+    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delall")
+    CONNECT_COMMAND = os.environ.get("CONNECT_COMMANDD", "connect")
+    DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMANDD", "disconnect")
+
+
+    # To record start time of bot
+    BOT_START_TIME = time.time()
