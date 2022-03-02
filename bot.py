12dad5317fe60b407a5cfac94c7305fa95ca81e7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-03 00:54:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 from pyrogram import Client
-from pymongo import MongoClient
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5219376297:AAFydXWjPg47TlZ4QnVkc2egji4mPMq0_2w")
 API_ID = int(os.environ.get("API_ID", "18891187"))
 API_HASH = os.environ.get("API_HASH", "7d120384f48b2a86fa2b9e9772a28af6")
-MONGO_URL = os.environ.get("mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 
 if __name__ == "__main__" :
     plugins = dict(
