8462635b422910b5fd1d5a2fc44f0f8669fda2f0
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-03 08:04:00 +0530

    Update levelup.py

--- plugins/modules/levelup.py
+++ plugins/modules/levelup.py
@@ -8,7 +8,7 @@ import os
 async def is_admins(chat_id: int):
     return [
         member.user.id
-        async for member in bot.iter_chat_members(
+        async for member in Client.iter_chat_members(
             chat_id, filter="administrators"
         )
     ]
