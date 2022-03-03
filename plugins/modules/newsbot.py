4cc4f6acbe048f5418a6cc640ffeac4c7b257b48
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-03 20:04:05 +0530

    Create newsbot.py

---
+++ plugins/modules/newsbot.py
@@ -0,0 +1,33 @@
+import requests
+from telethon import TelegramClient
+from telethon import events
+import os
+import logging
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+
+
+@bot.on(events.NewMessage(pattern="^/news ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    infintyvar = event.pattern_match.group(1)
+    main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
+    stuber = await event.reply(
+        f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
+    )
+    await stuber.edit("All News Has Been Sucessfully fetched, sendning to you.")
+    starknews = requests.get(main_url).json()
+    for item in starknews["data"]:
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        await bot.send_file(event.chat_id, sed1, caption=sedm)
