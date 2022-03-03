8f29b789324f7967b823493f4102ac755d8c1c1c
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-03 17:35:13 +0530

    Update linkremov.py

--- plugins/modules/linkremov.py
+++ plugins/modules/linkremov.py
@@ -4,9 +4,10 @@ from pyrogram import Client, filters
 
 
 
-@Client.on_message(filters.group & filters.regex("http") | filters.group & filters.regex("www") | filters.group & filters.regex("t.me"))
+@Client.on_message(filters.group & filters.regex("http") | filters.group & filters.regex("www") | filters.group & filters.regex("t.me") | filters.group & filters.regex("/" ) | filters.group & filters.regex("https") | filters.group & filters.regex("com") | filters.group & filters.regex("youtu.be"))     
 async def nolink(bot,message):
 	try:
 		await message.delete()
 	except:
 		return
+
