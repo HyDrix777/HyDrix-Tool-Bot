dec59eaf26f9512140803705f345e747ed529fe3
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-04 20:48:36 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -23,7 +23,7 @@ async def regex_1(bot, msg):
 
 @Client.on_message(filters.regex("hello") | filters.regex("Hello"))
 async def regex_2(bot, msg):
-    await msg.reply_text("Hello {msg.from_user.first_name} bro👋")
+    await msg.reply_text(f"Hello {msg.from_user.first_name} bro👋")
 
 
 
