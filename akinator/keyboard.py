c51738bbccabaa11590f69b0972f849747c38d2e
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-05 23:27:44 +0530

    Update keyboard.py

--- akinator/keyboard.py
+++ akinator/keyboard.py
@@ -1,14 +1,6 @@
 from akinator.strings import AKI_LANG_CODE, DEV_URL, GITHUB_URL
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-START_KEYBOARD = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Github', GITHUB_URL),
-            InlineKeyboardButton('Developer', DEV_URL)   
-        ]
-    ]
-)
 
 #Shows a bunch of buttons to change the language of the Akinator when playing.
 AKI_LANG_BUTTON = InlineKeyboardMarkup(
