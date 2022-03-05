9f971fbf4fb82ad5fd423c21db0034343b531e23
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-05 11:04:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-    USERNAME = os.environ.get("@HTGToolBot")
-    CAPTION = os.environ.get("false")
-    CUSTOM_TAG = os.environ.get("false")
+    USERNAME = os.environ.get("@my_channel")
+    CAPTION = os.environ.get("TRUE")
+    CUSTOM_TAG = os.environ.get("@HTGToolBot")
     if CUSTOM_TAG:
         custom_tag = " {" + CUSTOM_TAG + "}"
     else:
