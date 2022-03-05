a6f9d0578c8a590af99c4aa79ca84875e5cf0a06
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-06 01:09:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config(object):
-    AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    BOT_TOKEN = os.environ.get('bot_token', "5219376297:AAFydXWjPg47TlZ4QnVkc2egji4mPMq0_2w")
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
     CUSTOM_TAG = os.environ.get("@HTGToolBot")
