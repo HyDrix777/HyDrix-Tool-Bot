beacd63d6cdffa97cb171625c75f278fe6bd0304
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-05 10:37:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config(object):
+    USERNAME = os.environ.get('CHANNEL_USERNAME')
+    CAPTION = os.environ.get("DYNAMIC_CAPTION")
+    CUSTOM_TAG = os.environ.get("CUSTOM_TAG")
+    if CUSTOM_TAG:
+        custom_tag = " {" + CUSTOM_TAG + "}"
+    else:
+        custom_tag = " "
