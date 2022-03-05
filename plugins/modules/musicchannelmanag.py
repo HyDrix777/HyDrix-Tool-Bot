93afad2877bb88e3aed21d04a554a7b473d1d7e3
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-05 11:22:38 +0530

    Update musicchannelmanag.py

--- plugins/modules/musicchannelmanag.py
+++ plugins/modules/musicchannelmanag.py
@@ -13,7 +13,7 @@ from config import Config
 
 
     
-@Client.on_message(filters.group & filters.audio)
+@Client.on_message(filters.group & filters.audio | filters.channel & filters.audio)
 async def music(bot, m):
     fname = m.audio.file_name
     file = await m.download("temp/file.mp3")
