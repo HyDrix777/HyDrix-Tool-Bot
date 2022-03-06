b7e756b165d57fb4c2211d4d3d46fd380ff6541e
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-06 14:40:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
 class Config(object):
+    CONVERT_API = os.environ.get("GN4pzf2BHGr7nzHE")
+    MAX_FILE_SIZE = os.environ.get("8")
+    OWNER_ID = os.environ.get("OWNER_ID")
+    PDF_THUMBNAIL = "./thumbnail.jpeg"
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
     CUSTOM_TAG = os.environ.get("@HTGToolBot")
