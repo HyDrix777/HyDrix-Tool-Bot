946aefa1bbb3c1049817209d941500258080d421
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-07 11:24:39 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -15,3 +15,8 @@ TG_MAX_SELECT_LEN = 100
 LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", True))
 IMDB = bool((environ.get('IMDB', True)))
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001784386455')).split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001784386455))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '784589736').split()]
