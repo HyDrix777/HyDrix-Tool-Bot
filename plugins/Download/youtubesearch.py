509a13aa683f0876a30dc48f93013caa1f779463
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-09 15:32:01 +0530

    Update youtubesearch.py

--- plugins/Download/youtubesearch.py
+++ plugins/Download/youtubesearch.py
@@ -18,10 +18,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 10:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"✏️**Title** - {results[i]['title']}\n"
+            text += f"⌚**Duration** - {results[i]['duration']}\n"
+            text += f"👁️‍🗨️**Views** - {results[i]['views']}\n"
+            text += f"💻**Channel** - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
