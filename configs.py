f5f4ad6326a741f43ff53a7cd9948b947df08477
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-11 11:50:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,10 +1,7 @@
 import os
-import logging
 
 
-LOGGER = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
+
 
 
 
@@ -17,9 +14,5 @@ class Config(object):
         custom_tag = " {" + CUSTOM_TAG + "}"
     else:
         custom_tag = " "
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@HTGToolBot")
+    
 
-
-bot = Config.BOT_USERNAME
-class CMD(object):
-    RENAME = ["rename", f"rename@{bot}"]
