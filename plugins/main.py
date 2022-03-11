60435891b037ef8264b8add85f439f53993f301a
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-11 19:39:39 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -16,17 +16,6 @@ import lyricsgenius
 
 
 
-
-@Client.on_message(filters.group & filters.regex("hi") | filters.group & filters.regex("Hi"))
-async def regex_1(bot, msg):
-    await msg.reply_text(f"Hey {msg.from_user.first_name}🤗! How are you.😅")
-
-@Client.on_message(filters.regex("hello") | filters.regex("Hello"))
-async def regex_2(bot, msg):
-    await msg.reply_text(f"Hello {msg.from_user.first_name} bro👋")
-
-
-
 @Client.on_message(filters.private & filters.command("start"))
 async def start_message(bot, message):
     await message.reply_sticker(
