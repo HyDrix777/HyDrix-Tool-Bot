25e7c15df8e44345d6b7930bf75cc561e9d569d7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-11 15:00:41 +0530

    Create imgtopdf.py

---
+++ plugins/modules/imgtopdf.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image
+from pyrogram import Client,filters 
+from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
+
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
+
+@app.on_message(filters.command(['convert']))
+async def done(client,message):
+ images = LIST.get(message.from_user.id)
+
+ if isinstance(images, list):
+  del LIST[message.from_user.id]
+ if not images:
+  await message.reply_text( "No image !!")
+  return
+
+ path = f"{message.from_user.id}" + ".pdf"
+ images[0].save(path, save_all = True, append_images = images[1:])
+ 
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here is your PDF !!\n**PDF Created by:- @epusthakalaya_bots**")
+ os.remove(path)
