083deadffa32f1eed4169a8b48a8a4c51ea08245
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-12 21:18:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,11 +7,6 @@ import os
 
 
 class Config(object):
-        API_ID = int(os.environ.get("API_ID", "1234")
-        API_HASH = os.environ.get("API_HASH")
-        BOT_TOKEN = os.environ.get("BOT_TOKEN")
-        DATABASE_URL = os.environ.get("DATABASE_URL")
-
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
     CUSTOM_TAG = os.environ.get("@HTGToolBot")
