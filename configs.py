ac3b333d904ee5aab703fa5a6bc81780a5af6126
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-12 20:53:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,7 @@ import os
 
 
 class Config(object):
+    DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
     CUSTOM_TAG = os.environ.get("@HTGToolBot")
