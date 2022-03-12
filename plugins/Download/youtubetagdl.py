43f78d5bc88d10bbc0b36806a6613938b242f0c0
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-12 19:54:38 +0530

    Update youtubetagdl.py

--- plugins/Download/youtubetagdl.py
+++ plugins/Download/youtubetagdl.py
@@ -10,7 +10,7 @@ from YoutubeTags import videotags
 
 
 
-@Client.on_message(filters.private & filters.reply("tag")
+@Client.on_message(filters.group & filters.regex("https://www.youtube.com") | filters.regex("http://www.youtube.com") | filters.regex("https://youtu.be/") | filters.regex("https://www.youtu.be/") | filters.regex("http://www.youtu.be/"))
 async def tag(bot, message):
     link = str(message.text)
     tags = videotags(link) 
