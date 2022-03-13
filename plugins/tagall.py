01c17fc31836e8c88552cbfe7de9a191d013d3ef
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-13 14:30:13 +0530

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -1,5 +1,5 @@
 import os, logging, asyncio
-from pyrogram import Client filters
+from pyrogram import Client
 from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
