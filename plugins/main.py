0f32d4a76c00a9385088b908f171c9210872ec5b
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-14 17:50:07 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -48,6 +48,9 @@ async def start_message(bot, message):
             )
         else:
             logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
+    await message.reply_sticker(
+        sticker="CAACAgQAAxkBAAIg5WIsoeHJ6NPJQWVJxYccrii6UHZdAAKgCAACp0h3Bfo4mn6VPDjmHgQ"             
+    )
     await message.reply_text(
         text=f"✨ **ʜᴇʟʟᴏ** ||{message.from_user.mention}||!\n\n💭 ɪ'ᴍ **ʜʏᴅʀɪx ᴛᴏᴏʟs ʙᴏᴛ** [🛠️](https://telegra.ph/file/ea82bbb4deebdbf9d68e8.jpg)\n\n💡 Fɪɴᴅ ᴏᴜᴛ ᴀʟʟ ᴛʜᴇ Bᴏᴛ's ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ\nʜᴏᴡ ᴛʜᴇʏ ᴡᴏʀᴋ ʙʏ ᴄʟɪᴄᴋɪɴɢ ᴏɴ ᴛʜᴇ » 📚\nCᴏᴍᴍᴀɴᴅs ʙᴜᴛᴛᴏɴ!",
         reply_markup=InlineKeyboardMarkup( [[
