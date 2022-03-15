e819dc48c9d7672775392080b9369506105471ea
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-16 00:18:05 +0530

    Create uptime.py

---
+++ plugins/modules/uptime.py
@@ -0,0 +1,17 @@
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters
+from infog import BOT_USERNAME
+
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
