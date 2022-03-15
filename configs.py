9e96baa4e079b6b73217de0ea2f698a672fcf585
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-15 15:14:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,7 @@ import os
 
 
 class Config(object):
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
     CUSTOM_TAG = os.environ.get("@HTGToolBot")
