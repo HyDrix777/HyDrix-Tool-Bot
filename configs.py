9b7689379649130dc26550538e96378601ab76df
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-17 12:41:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,9 @@ import os
 
 
 class Config(object):
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
