a7390fb5183e4699924e5b2b0fe6ea593eb6bcdd
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-19 19:50:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,6 +8,7 @@ import os
 
 class Config(object):
 
+    DATABASE_URL = os.environ.get("mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
     USERNAME = os.environ.get("@my_channel")
     CAPTION = os.environ.get("TRUE")
