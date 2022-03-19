8258d116838a298435df98c37be2a46110daa0ad
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-19 11:41:17 +0530

    Create channelban.py

---
+++ plugins/modules/channelban.py
@@ -0,0 +1,36 @@
+import os
+from telebot.types import Message
+from telebot import TeleBot
+from dotenv import load_dotenv
+from pyrogram import Client filters
+
+load_dotenv()
+
+
+
+
+@Client.message_handler(func=lambda message: True, chat_types=['supergroup'])
+def channels_ban(updates: Message):
+    if updates.sender_chat:
+        bot.delete_message(
+            chat_id=updates.chat.id,
+            message_id=updates.message_id
+        )
+        bot.ban_chat_sender_chat(
+            chat_id=updates.chat.id,
+            sender_chat_id=updates.sender_chat.id
+        )
+
+
+@Client.message_handler(commands=["start"], chat_types=['private'])
+def start_message(updates: Message):
+    bot.send_message(
+        updates.chat.id,
+        f'Hello, [{updates.from_user.first_name}](tg://user?id={updates.from_user.id})\n\n'
+        f'This is a telegram bot. It does not allow channels to send messages to the telegram supergroup.\n\n'
+        f'Contact [Abhijith N T](tg://user?id=429320566) if you have any problems or create a '
+        f'[new issue](https://github.com/AbhijithNT/GroupChannelHandler/issues/new) on GitHub',
+        parse_mode='Markdown'
+    )
+
+
