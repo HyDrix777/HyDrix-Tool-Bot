76a216e8fd496a2876315e682953684578f14fa7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-24 00:10:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
 # Unziper module
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    DOWNLOAD_START = " 📥DownloadinG📥 \n\nWait⏳ untill it completed."
+    DOWNLOAD_START = " 📥 Downloading \n\nWait⏳ untill it completed."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists."
@@ -10,12 +10,12 @@ class Translation(object):
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
 
 # Renamer module
-    CUSTOM_CAPTION_UL_FILE = "@htgtoolbot"
-    DOWNLOAD_FILE = " 📥DownloadinG📥 File "
+    CUSTOM_CAPTION_UL_FILE = "Uploaded by: @HTGToolBot"
+    DOWNLOAD_FILE = " 📥 Downloading File "
     FILE_NOT_FOUND = "Error, File not Found!!"
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    UPLOAD_START = " 📤UploadinG📤 "
-    AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN : https://t.me/tg_galaxy\nchannel"
+    UPLOAD_START = " 📤 Uploading File "
+    AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN MY GROUP❤️: https://t.me/Music_Galaxy_Dl"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /ren with custom thumbnail support"
 
 
