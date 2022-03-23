57a3bcccf7433b3ecf83d61f871d8b51a19f7281
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-23 20:07:49 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,12 @@
+class Translation(object):
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    DOWNLOAD_START = " 📥DownloadinG📥 \n\nWait⏳ untill it completed."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/Htgtoolbot'>@Htgtoolbot</a>"
+    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
+    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."""
+
+
+
