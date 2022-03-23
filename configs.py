e34067568662a2e1bfb00b52af74a9599d954f16
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-23 19:56:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,8 @@ import os
 
 
 class Config(object):
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
 
     DATABASE_URL = os.environ.get("mongodb+srv://hydra92074:hydra92074@cluster0.ktfkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
