0e5359eae083ffcc3bb56e3de389035ad4b017a0
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-24 01:35:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -18,6 +18,9 @@ You can use /rename command after receiving file to rename it with custom thumbn
     AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN MY GROUP❤️: https://t.me/Music_Galaxy_Dl"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
 
-
-
+# Go fileupload module
+    GO_FILE_UPLOAD = " 📤 Uploading \n\n To  gofile.io "
+    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\nJoin : @HTgToolbot"
+    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
+    
 
