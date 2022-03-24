3cb3e6937470afd27d543101749c22a562158a08
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-24 13:00:15 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -27,4 +27,10 @@ You can use /rename command after receiving file to rename it with custom thumbn
     ANNO_UPLOAD = " 📤 Uploading \n\n To  anonfiles.com "
     AFTER_GET_LINK = " anonfiles.com\n\n<b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b> 🔗 Link :</b> <code>{}</code>\n\nUploaded by: @HTgToolbot\nJoin my group: @Music_Galaxy_Dl"
     
-
+# Costom thumbnail
+    CUSTOM_CAPTION_UL_FILE = "Uploaded by: @HTgToolbot"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    
