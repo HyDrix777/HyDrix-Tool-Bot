6d96fd28edd5bd158e87323d530ea0316f86c205
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-24 09:42:42 +0530

    Update anon.py

--- plugins/modules/anon.py
+++ plugins/modules/anon.py
@@ -23,8 +23,8 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.display_progress import progress_for_pyrogram
-from helper_funcs.ran_text import random_char
+from database.display_progress import progress_for_pyrogram
+from database.ran_text import random_char
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
