c7a2330be7c03be2adb9c3c933ed3f4f8f4c07fe
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-26 23:02:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,15 @@ import os
 
 
 class Config(object):
+    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
+    CREATOR_REQUIRED = """❗<b>You have To Be The Group Creator To Do That.</b>"""
+    INPUT_REQUIRED = "❗ **Arguments Required**"
+    KICKED = """✔️ Successfully Kicked {} Members According To The Arguments Provided."""
+    ADMIN_REQUIRED = """❗<b>എന്നെ Admin ആക്കത്ത സ്ഥലത്ത് ഞാൻ നിക്കില്ല പോകുവാ Bii..Add Me Again with all admin rights.</b>"""
+    START_KICK = """🚮 Removing Inactive Members This May Take A While..."""
+    FETCHING_INFO = """<b>ഇപ്പൊ എല്ലാം അടിച്ചുമാറ്റി തരാം...</b>"""
+    STATUS = """{}\n<b>Chat Member Status</b>**\n\n```<i>Recently``` - {}\n```Within Week``` - {}\n```Within Month``` - {}\n```Long Time Ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}</i>"""
+
     DEF_WATER_MARK_FILE = "Uploaded by: @HTgToobot"
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
