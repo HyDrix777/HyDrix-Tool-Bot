b9258a37a224c8d8f0a43ae91362849ad364b1b9
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-27 00:45:51 +0530

    Update ban.py

--- plugins/modules/ban.py
+++ plugins/modules/ban.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
-from plugins.helper_functions.admin_check import admin_check
-from plugins.helper_functions.extract_user import extract_user
-from plugins.helper_functions.string_handling import extract_time
+from plugins.admin_check import admin_check
+from plugins.extract_user import extract_user
+from plugins.string_handling import extract_time
 
 
 @Client.on_message(filters.command("ban"))
