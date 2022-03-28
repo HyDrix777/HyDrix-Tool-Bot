cb32f7dd5952282b6e072b84ae0d10a8e847bf01
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-03-29 00:09:08 +0530

    Update fun.py

--- plugins/modules/fun.py
+++ plugins/modules/fun.py
@@ -42,6 +42,27 @@ async def throw_dart(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
+# Break----
+
+# EMOJI CONSTANTS
+BREAK_YOUR_LOVE = "💔"
+# EMOJI CONSTANTS
+
+
+@Client.on_message(
+    filters.command(["break", "thepp"])
+)
+async def break_dart(client, message):
+    """ /break an @AnimatedDart """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=BREAK_YOUR_LOVE,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
 
 # LUCK------------
 
