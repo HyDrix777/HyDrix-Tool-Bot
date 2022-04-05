96f9a13a824489627573ca6b740b8a53fff08787
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-05 14:23:45 +0530

    Update infog.py

--- infog.py
+++ infog.py
@@ -3,6 +3,28 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 import os
 
+from pyromod import listen
+import logging
+from pyrogram import Client, idle
+
+
+
+# LOGGING INFO: DEBUG
+logging.basicConfig(
+    level = logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+# GLOBAL VARIABLES
+PDF = {}            # save images for generating pdf
+PROCESS = []        # to check current process
+invite_link = None
+
+# PLUGIN DIRECTORY
+plugin = dict(
+    root = "plugins"
+)
 
 
 
