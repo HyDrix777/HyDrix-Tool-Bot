cb24d8bb567f7bf56150979160d453057d2560c7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-07 20:50:11 +0530

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -1,5 +1,5 @@
 from driver.queues import QUEUE, clear_queue, get_queue, pop_an_item
-from driver.veez import Client, call_py
+from bot import Client, call_py
 from infog import IMG_4
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
