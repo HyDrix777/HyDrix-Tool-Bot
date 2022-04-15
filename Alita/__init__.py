35faad0aac53901bc9a8426b10969f4a3bcbddd9
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-16 00:22:22 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -54,7 +54,7 @@ LOGGER.info("|     Hydrix_Robot     |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
-LOGGER.info("Source Code: https://github.com/DivideProjects/Alita_Robot\n")
+LOGGER.info("Source Code: https://github.com/Hydrayt777/HyDrix-Tool-Bot\n")
 
 # Account Related
 BOT_TOKEN = Config.BOT_TOKEN
