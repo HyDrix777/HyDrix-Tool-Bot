761a8514623f4ea2158a1712209461806f9e10bd
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-16 20:10:53 +0530

    Update urlshortner.py

--- plugins/urlshortner.py
+++ plugins/urlshortner.py
@@ -13,7 +13,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='My group', url='https://t.me/Music_Galaxy_Dl')
+        InlineKeyboardButton(text='My group', url='https://t.me/songdownload_group')
         ]]
     )
 
