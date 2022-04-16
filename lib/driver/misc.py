39cd7ebf59d24a52780f8cbb891443a5135650f7
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-16 16:41:12 +0530

    Create misc.py

---
+++ lib/driver/misc.py
@@ -0,0 +1,163 @@
+import os
+from datetime import datetime
+
+import psutil
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pytgcalls.exceptions import GroupCallNotFound
+
+from lib.config import BOTLOG_CHATID
+from lib.helpers.decorators import blacklist_users, sudo_users
+from lib.helpers.ffmpeg_audio import get_audio
+from lib.tg_stream import app as USER
+from lib.tg_stream import call_py
+
+
+@Client.on_message(filters.command("pings"))
+@blacklist_users
+async def ping_(client: Client, message: Message):
+    start = datetime.now()
+    msg = await message.reply_text("`Latensi`")
+    end = datetime.now()
+    latency = (end - start).microseconds / 1000
+    await msg.edit(f"**Latency:** `{latency} ms`")
+
+
+@Client.on_message(filters.command("repo"))
+@blacklist_users
+async def repo(client, message):
+    repo = "https://github.com/galihmrd/Group-calls-video"
+    license = "https://github.com/galihmrd/Group-calls-video/blob/stream/beta/LICENSE"
+    await message.reply(
+        f"**Source code:** [Here]({repo})\n**License:** [GPL-3.0 License]({license})",
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_message(filters.command("pause"))
+@blacklist_users
+async def pause(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await call_py.pause_stream(chat_id)
+        await message.reply(f"**{type} stream paused!**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found!")
+
+
+@Client.on_message(filters.command("resume"))
+@blacklist_users
+async def resume(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await call_py.resume_stream(chat_id)
+        await message.reply(f"**{type} stream resumed!**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found!")
+
+
+@Client.on_message(filters.command("stop"))
+@blacklist_users
+async def stopped(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await call_py.leave_group_call(chat_id)
+        await message.reply(f"**{type} stream stopped!**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found")
+
+
+@Client.on_message(filters.command("volume"))
+@sudo_users
+async def change_volume(client, message):
+    range = message.command[1]
+    chat_id = message.chat.id
+    try:
+        await call_py.change_volume_call(chat_id, volume=int(range))
+        await message.reply(f"**Volume changed to:** `{range}%`")
+    except Exception as e:
+        await message.reply(f"**Error:** {e}")
+
+
+@Client.on_message(filters.command(["logs", "log"]))
+@sudo_users
+async def logfile(client, message):
+    try:
+        await client.send_document(document="log.txt", chat_id=int(BOTLOG_CHATID))
+        await message.reply("I've send the log on Bot Log's")
+    except BaseException:
+        await message.reply("**Oops!!**\nsomething is wrong")
+
+
+# PMpermit
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+    await USER.send_message(
+        message.chat.id, f"Hello {message.from_user.mention} How are you?"
+    )
+
+
+# Start message
+@Client.on_message(filters.command("st"))
+@blacklist_users
+async def st(client, message):
+    await message.reply(f"Hello {message.from_user.mention} how are you?")
+
+
+# System info
+@Client.on_message(filters.command("sysd"))
+@sudo_users
+async def sysinfo(client, message):
+    cpuUsage = psutil.cpu_percent(interval=0.5)
+    diskUsage = psutil.disk_usage("/").percent
+    msg = await message.reply("`Processing...`")
+    await msg.edit(
+        f"**System Information**\n**CPU:** {cpuUsage}%\n**Disk:** {diskUsage}%"
+    )
+
+
+# Converter
+@Client.on_message(filters.command("videotoaudio"))
+@blacklist_users
+async def videotoaudio(client, message):
+    try:
+        replied = message.reply_to_message
+        outName = f"{message.from_user.first_name}_{replied.video.file_id}.mp3"
+        if replied.video or replied.document:
+            msgDialog = await message.reply("`Downloading from telegram server...`")
+            inputName = await client.download_media(replied)
+            await msgDialog.edit("`Converting via Ffmpeg...`")
+            get_audio(inputName, outName)
+            await msgDialog.edit("`Uploading to telegram server...`")
+            await message.reply_audio(
+                outName,
+                title=outName,
+                caption=f"**Requested by:** {message.from_user.mention}",
+            )
+            try:
+                await msgDialog.delete()
+                os.remove(outName)
+            except BaseException:
+                pass
+    except Exception as e:
+        await message.reply(
+            f"**Input not found:** Reply command to video file!\n**Logs:** `{e}`"
+        )
