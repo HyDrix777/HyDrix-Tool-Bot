46d4624cc605a4f2017d6e6aa56a2d1128c6c1d9
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-16 13:37:41 +0530

    Create pstream.py

---
+++ lib/helpers/pstream.py
@@ -0,0 +1,39 @@
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import AudioImagePiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import HighQualityVideo
+
+from lib.tg_stream import call_py
+
+
+async def pstream(chat_id, file, audio=None):
+    if audio:
+        await call_py.join_group_call(
+            chat_id,
+            AudioImagePiped(
+                file,
+                "./etc/banner.png",
+                video_parameters=HighQualityVideo(),
+            ),
+            stream_type=StreamType().local_stream,
+        )
+    else:
+        await call_py.join_group_call(
+            chat_id,
+            AudioVideoPiped(
+                file,
+                video_parameters=HighQualityVideo(),
+            ),
+            stream_type=StreamType().local_stream,
+        )
+
+
+async def pstream_audio(chat_id, file, thumb):
+    await call_py.join_group_call(
+        chat_id,
+        AudioImagePiped(
+            file,
+            thumb,
+            video_parameters=HighQualityVideo(),
+        ),
+        stream_type=StreamType().local_stream,
+    )
